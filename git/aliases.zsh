023a9a384a6fc7c36390477bd5e3978fd5fa465d
Joshua Rubin <jrubin@zvelo.com>
2013-07-10 01:44:52 -0600

    stop using the slow hub git wrapper
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,10 +1,4 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git="noglob $hub_path"
-fi
+alias git="noglob git"
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
