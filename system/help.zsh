eab62806eba361a9eafb868f5bb7c6df1307c960
Joshua Rubin <jrubin@zvelo.com>
2014-10-28 20:42:16 -0600

    continue speeding up zsh
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- system/help.zsh
+++
@@ -1,5 +0,0 @@
-if [[ "$OSTYPE" != darwin* ]]; then return 1; fi
-
-unalias run-help 2> /dev/null
-autoload run-help
-export HELPDIR="$(brew --prefix)/share/zsh/helpfiles"
--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,8 +1,2 @@
-# matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
-# pasting with tabs doesn't perform completion
-zstyle ':completion:*' insert-tab pending
-
 if (( $+commands[npm] )); then source <(npm completion 2> /dev/null); fi
 if (( $+commands[nohap] )); then source <(nohap completion); fi
